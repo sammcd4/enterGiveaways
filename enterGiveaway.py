48a391fa24097c7bd12e4697b1d76ac6fa563a56
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-10-08 21:23:47 -0400

    Added people - simply implemented

--- enterGiveaway.py
+++ enterGiveaway.py
@@ -1,5 +1,11 @@
+import giveaway.person as gp
 import giveaway.manager as gm
 
+# Choose people to enter giveaways for
+people = [gp.Person('Sam', 'McDonald', 'whitehops@gmail.com')]
+# people.append(gp.Person('Melissa', 'McDonald', 'melis.lott@gmail.com'))
+# people.append(gp.Person('Julia', 'McDonald', ''))
+
 # Choose a list of urls of giveaways to enter
-gm = gm.GiveawayManager('GiveawayInfo.txt')
+gm = gm.GiveawayManager('GiveawayInfo.txt', people)
 gm.run()
