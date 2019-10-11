ad6cad4ddf4be6c504f60ad24e0af3a2b76340d1
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-10-11 10:23:04 -0400

    Entered giveaways correctly

--- enterGiveaway.py
+++ enterGiveaway.py
@@ -2,9 +2,9 @@ import giveaway.person as gp
 import giveaway.manager as gm
 
 # Choose people to enter giveaways for
-# people = [gp.Person('Sam', 'McDonald', 'whitehops@gmail.com')]
+people = [gp.Person('Sam', 'McDonald', 'whitehops@gmail.com')]
 # people.append(gp.Person('Melissa', 'McDonald', 'melis.lott@gmail.com'))
-people = []
+# people = []
 people.append(gp.Person('Julia', 'McDonald', 'juliamcdonald88@gmail.com'))
 
 # Choose a list of urls of giveaways to enter
