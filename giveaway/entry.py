6cc83a2abca3dbaacc43fb564531e003f18fd161
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-10-18 09:23:37 -0400

    More error handling

--- giveaway/entry.py
+++ giveaway/entry.py
@@ -80,11 +80,25 @@ class GiveawayEntry:
 
     def fill_textbox(self, text_id, text_str):
         try:
-            last_name = self._driver.find_element_by_id(text_id)
-            last_name.send_keys(text_str)
+            element = self._driver.find_element_by_id(text_id)
+            element.send_keys(text_str)
         except:
             print('\tUnable to fill {} textbox with {}'.format(text_id, text_str))
         time.sleep(self.delay + self.delay_noise * random.random())
+
+    def fill_textbox_and_submit(self, text_id, text_str):
+        try:
+            element = self._driver.find_element_by_id(text_id)
+            element.send_keys(text_str)
+
+            if not self.actually_enter:
+                print('\tSimulating submission')
+            else:
+                element.submit()
+        except:
+            print('\tUnable to fill {} textbox with {}'.format(text_id, text_str))
+
+        self.confirm_submission()
 
     def click_submit_button(self, button):
 
@@ -97,9 +111,24 @@ class GiveawayEntry:
                 self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
                 button.click()
 
-            # pause for short time to manually verify submission, if desired
-            if not self.noDelay:
-                time.sleep(5)
+        self.confirm_submission()
+
+    def submit_with_enter(self, button):
+        if not self.actually_enter:
+            print('\tSimulating submission')
+        else:
+            try:
+                button.submit()
+            except:
+                self._driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                button.click()
+
+        self.confirm_submission()
+
+    def confirm_submission(self):
+        # pause for short time to manually verify submission, if desired
+        if not self.noDelay:
+            time.sleep(5)
 
         try:
             self._driver.close()
@@ -140,11 +169,4 @@ class LeitesCulinariaEntry(GiveawayEntry):
     def fill_and_submit(self, person):
         # Fill form
         self.fill_textbox('giveaway_entry_name', person.full_name)
-        self.fill_textbox('giveaway_entry_email', person.email)
-
-        # Submit form
-        try:
-            button = self._driver.find_element_by_class_name('giveaway-field')
-        except:
-            print('\tUnable to find submit element')
-        self.click_submit_button(button)
+        self.fill_textbox_and_submit('giveaway_entry_email', person.email)
