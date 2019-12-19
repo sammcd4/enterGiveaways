61c42a0466d11a4c67dc552f93cf9e71c8d450ef
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-12-18 23:30:25 -0500

    Still working on gathering

--- giveaway/gather.py
+++ giveaway/gather.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class GiveawayGatherer:
     # Class used to automate gathering giveaway sites and populate the GiveawayInfo.txt file
 
@@ -10,11 +11,31 @@ class GiveawayGatherer:
         print('Gathering all new giveaways from Steamy Kitchen...')
         giveaway_page = 'https://steamykitchen.com/current-giveaways'
 
-        html = requests.get(giveaway_page).text
+        soup = self.getSoup(giveaway_page)
 
-        soup = BeautifulSoup(html, "lxml")
-        giveaway_posts = soup.findAll("article", {"class": "item skg-archive-post"})
+        giveaway_posts = soup.findAll("div", {"class": "item skg-archive-post"})
         for gp in giveaway_posts:
-            print("Giveaway link: " + gp.find('a').text)
-            # print("Address: " + r.find("div", {'class': 'address'}).text)
-            # print("Website: " + r.find_all("div", {'class': 'pageMeta-item'})[3].text)
+            giveaway_link = gp.find('a').get('href')
+            print("Giveaway link: " + giveaway_link)
+
+            # extract giveaway expiration from webpage with another soup
+            soup_giveaway = self.getSoup(giveaway_link)
+
+            form = soup_giveaway.findAll("div", {'class': 'skg-meta'})
+
+            # form = soup_giveaway.findAll("div", {'class': 'skg-submission-form-container'})
+
+            # expiration_paragraph = form.skg_submission_form
+            for f in form:
+                giveaway_expiration = f.find("div", {'class': 'skg-meta'})
+                print('\tExpires on: ' + giveaway_expiration)
+                # print("Address: " + r.find("div", {'class': 'address'}).text)
+                # print("Website: " + r.find_all("div", {'class': 'pageMeta-item'})[3].text)
+
+        #for link in soup.find_all('a'):
+        #   print(link.get('href'))
+
+    def getSoup(self, link):
+        html = requests.get(link).text
+        soup = BeautifulSoup(html, "html.parser")
+        return soup
