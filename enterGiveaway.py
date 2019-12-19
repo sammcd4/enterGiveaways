abcf483dedfd9ed2b4b245b69f9bccea74238dd4
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2019-12-18 23:30:25 -0500

    Still working on gathering

--- enterGiveaway.py
+++ enterGiveaway.py
@@ -1,12 +1,20 @@
 import giveaway.person as gp
 import giveaway.manager as gm
 
-# Choose people to enter giveaways for
-people = [gp.Person('Sam', 'McDonald', 'whitehops@gmail.com')]
-# people.append(gp.Person('Melissa', 'McDonald', 'melis.lott@gmail.com'))
-# people = []
-people.append(gp.Person('Julia', 'Larson', 'juliamcdonald88@gmail.com'))
+# Update all giveaways
+if False:
+    from giveaway.gather import GiveawayGatherer
+    g = GiveawayGatherer()
+    g.gather()
 
-# Choose a list of urls of giveaways to enter
-gm = gm.GiveawayManager('GiveawayInfo.txt', people)
-gm.run()
+else:
+
+    # Choose people to enter giveaways for
+    people = [gp.Person('Sam', 'McDonald', 'whitehops@gmail.com')]
+    # people.append(gp.Person('Melissa', 'McDonald', 'melis.lott@gmail.com'))
+    # people = []
+    people.append(gp.Person('Julia', 'Larson', 'juliamcdonald88@gmail.com'))
+
+    # Choose a list of urls of giveaways to enter
+    gm = gm.GiveawayManager('GiveawayInfo.txt', people)
+    gm.run()
