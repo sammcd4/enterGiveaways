f502ca428f8471a8168b8ce36d9edb6530e33f15
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-04-27 00:15:37 -0600

    Added ability to open new incognito tab for additional manual entries

--- openGiveaways.py
+++ openGiveaways.py
@@ -1,10 +1,26 @@
 import webbrowser
 import time
+import os
 
-giveaway_info = 'GiveawayInfo.txt'
-with open(giveaway_info, 'r') as f:
-    for line in f:
-        url = line[line.find('http'):]
-        print('Opening {}'.format(url))
-        webbrowser.open_new_tab(url)
-        time.sleep(20)
+
+def open_incognito_tab(url):
+    system_command = "open -na \"Google Chrome\" --args -incognito {}".format(url)
+    #print(system_command)
+    os.system(system_command)
+
+
+if __name__ == "__main__":
+    use_incognito = True
+    giveaway_info = 'GiveawayInfo.txt'
+    with open(giveaway_info, 'r') as f:
+        for line in f:
+            url = line[line.find('http'):]
+            print('Opening {}'.format(url))
+            if use_incognito:
+                #chrome_path = 'open -na /Applications/Google\ Chrome.app %s --args -incognito'
+                #webbrowser.get(chrome_path).open_new_tab(url)
+
+                open_incognito_tab(url)
+            else:
+                webbrowser.open_new_tab(url)
+            time.sleep(15)
